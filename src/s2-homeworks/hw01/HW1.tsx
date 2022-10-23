462120b30de36c02c2f0e5ddee5dee6b034dd59b
EgorZapatylak <zapatylak96a@gmail.com>
2022-10-23 22:26:19 +0300

    Fixed some bag in homework

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -25,22 +25,22 @@ export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Ivan',  // можно менять
+        name: 'Igor',  // можно менять
     },
     message: {
         text: 'Hello, she didn’t do anything and rested all day, how are you?', // можно менять
-        time: '09:00', // можно менять
+        time: '09:01', // можно менять
     },
 }
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Igor', // можно менять
+        name: 'Ivan', // можно менять
     },
     message: {
         text: 'Hello, how are you, what did you do yesterday?', // можно менять
-        time: '09:01', // можно менять`
+        time: '09:00', // можно менять`
     },
 }
 
